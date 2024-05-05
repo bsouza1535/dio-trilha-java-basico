f9923bf5f337a2b45b32b4a19f1266e89ff288a6
Bruno Souza <76167904+bsouza1535@users.noreply.github.com>
2024-05-05 17:06:01 -0300

    Update TiposVariaveis.java

--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        //Como declarar double
+        double salariominimo = 1300;
+        //Para declarar float tem que colocar f no fim e para declarar constantes tem que usar final e caixa alta.
+        final float PI = 3.14f;
+        //Declarar string é necessário colocar o valor entre parênteses.
+        String meuNome = "Bruno Souza";
     }
 }
